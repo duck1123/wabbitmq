a70e68a57545b9dccf34938c1c342a6d6028dcfb
Daniel E. Renfer <duck@kronkltd.net>
2011-09-28 09:40:33 -0400

    dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/mefesto/wabbitmq"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
                  [com.rabbitmq/amqp-client "2.2.0"]])
